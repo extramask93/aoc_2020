c938b247165cee67bee93c1ddee2d85e77e9daca
Damian Jozwiak <damian.jozwiak@partner.bmw.de>
2025-10-25 15:47:24 +0200

    Add day1 part 2

--- day1.py
+++ day1.py
@@ -1,18 +1,29 @@
 from pathlib import Path
+from typing import List
+from itertools import permutations
 
-def read_input_values(path: Path) -> str:
+def read_input_values(path: Path) -> List[str]:
     with path.open("r") as file:
-        for line in file:
-            yield line.strip()
+        return file.readlines()
         
-
-
-if __name__ == "__main__":
-    values = (int(x) for x in read_input_values(Path("inputs") / "day1.txt"))
+def target_addends(values: List[int], target: int) -> tuple[int]| None:
     seen = set()
     for val in values:
-        complement = 2020-val
+        complement = target-val
         if complement in seen:
-            print(complement*val)
-            break
+            return val, complement
         seen.add(val)
+    return None
+
+if __name__ == "__main__":
+    values = [int(x) for x in read_input_values(Path("inputs") / "day1.txt")]
+    a,b = target_addends(values, 2020)
+    print(f"First = {a*b}")
+    for i, value in enumerate(values):
+        addends = target_addends(values[i:], 2020-value)
+        if addends is not None:
+            print(f"Second = {value*addends[0]*addends[1]}")
+            break
+            
+
+        
